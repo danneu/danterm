98d595481fd714141260fe5aea8ce567fd08b497
danneu <529580+danneu@users.noreply.github.com>
2026-03-06 03:14:52 +0000

    nix: update package.nix to v0.0.6

--- package.nix
+++ package.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchzip }:
 let
-  version = "0.0.5";
+  version = "0.0.6";
 in
 stdenv.mkDerivation {
   pname = "danterm";
   inherit version;
   src = fetchzip {
     url = "https://github.com/danneu/danterm/releases/download/v${version}/DanTerm-${version}.zip";
-    sha256 = "sha256-fI5fWPsGEhHvm1/YCkGVoUxpvgLoLC38kmXHYb39DTo=";
+    sha256 = "sha256-bpyxmGTG2qducF1vggxp3f3XnlXsVkvLzfLqWevSBK0=";
   };
   installPhase = ''
     mkdir -p $out/Applications/DanTerm.app
