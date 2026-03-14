342ad18cfcbf5a48affac9e730405279968ac552
danneu <529580+danneu@users.noreply.github.com>
2026-03-14 23:44:20 +0000

    nix: update package.nix to v0.0.28

--- package.nix
+++ package.nix
@@ -1,6 +1,6 @@
 { lib, stdenvNoCC, fetchzip }:
 let
-  version = "0.0.27";
+  version = "0.0.28";
 in
 # stdenvNoCC: no compiler needed — we install a pre-built .app bundle from GitHub Releases.
 stdenvNoCC.mkDerivation {
@@ -8,7 +8,7 @@ stdenvNoCC.mkDerivation {
   inherit version;
   src = fetchzip {
     url = "https://github.com/danneu/danterm/releases/download/v${version}/DanTerm-${version}.zip";
-    sha256 = "sha256-MiL6fv4IX5bfqK6obd9J6mBIZW3lhU5IpFMU9vqa/pA=";
+    sha256 = "sha256-3mXkV48LOzaoGMPXrwFiI9suAf2MjzHkUTQ86HRpS0U=";
   };
   installPhase = ''
     mkdir -p $out/Applications/DanTerm.app
